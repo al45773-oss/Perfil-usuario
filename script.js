0232965a04fcc4973aae2bf25275783581f7ff8a
al45773-oss <al45773@un.edu.mx>
2025-12-02 10:39:25 -0600

    java cambio de hombre a mujer

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ let esMasculino = true;
         const nombre = document.getElementById("nombre");
         const ocupacion = document.getElementById("ocupacion");
 
-   
+        card.addEventListener("click", () => {
+            if (esMasculino) {
+                
+                //Se cambia a Femenino tocando el nombre o la ocupación
+                nombre.textContent = "Andy-Pierinn Gignac";
+                ocupacion.textContent = "Futbolista Delantera Profesional";
+                alert("El perfil ha cambiado a femenino.");
+            } else {
+                
+                //Se regresa al nombre original
+                nombre.textContent = "André-Pierre Gignac";
+                ocupacion.textContent = "Futbolista Delantero";
+                alert("El perfil ha cambiado");
+            }
+
+            esMasculino = !esMasculino; 
+        });
